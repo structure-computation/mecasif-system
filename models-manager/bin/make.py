32b4d79a985650d8991c9e9362ff027903a00f41
Clement Pianon <c.pianon@structure-computation.com>
2016-06-02 14:48:48 +0200

    changed global script to run hub+organs-manager; added global model.js file

--- models-manager/bin/make.py
+++ models-manager/bin/make.py
@@ -1,5 +1,14 @@
 #execfile( "./concat_js.py" )
 from concat_js import *
 
+models = []
+
 for p in os.listdir( "models.coffee" ):
     concat_js( "models.coffee/" + p, "gen/" + p + ".js" )
+    models.append("gen/" + p + ".js")
+
+exec_cmd( "echo > gen/models.js " )
+
+for m in sorted(models):
+    exec_cmd( "cat gen/models.js " + m + " > gen/models_tmp.js" )
+    exec_cmd( "mv gen/models_tmp.js gen/models.js" )
